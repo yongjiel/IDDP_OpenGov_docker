70e5cca2b2d8d507ba81211d3a150f86c84caced
Jack Liang <jack.liang@gov.ab.ca>
2017-05-16 13:11:56 -0600

    Move rm commands into sh file instead of in Dockfile.

--- db/pg_restore.sh
+++ db/pg_restore.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+echo "RESTORE database..."
 psql -U postgres < /ckan11.dump 
+rm -rf /ckan11.dump
--- db/set_permission.sh
+++ db/set_permission.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+echo "SET PERMISSION for datastore..."
 cat /set_permission.sql | psql -U postgres --set ON_ERROR_STOP=1 
+rm -rf /set_permission.sql
